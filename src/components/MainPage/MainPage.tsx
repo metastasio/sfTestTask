47ae2de8e266316e26dbcee7f23bfbf0a84e5d56
metastasio <myaldzina.m@gmail.com>
2024-06-09 13:30:57 +0300

    Applying styles to Main

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -9,7 +9,7 @@ export const MainPage = () => {
       <Header />
       <MainMenu />
       <main className='main_page'>
-        <div className='main_page_adv1'>
+        <section className='main_page_adv1'>
           <p className='main_page_adv1_title'>
             До 31 декабря любой курс со скидкой 20%
           </p>
@@ -17,12 +17,38 @@ export const MainPage = () => {
             До конца года у вас есть уникальная возможность воспользоваться
             нашей новогодней скидкой 20% на любой курс!
           </p>
-        </div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        </section>
+
+        <section className='main_next_lesson'>
+          <p className='main_next_lesson_title'>
+            Следующее занятие начнется через:
+          </p>
+          <p className='main_next_lesson_timer'>SET TIMER!!</p>
+          <button className='main_next_lesson_button'>Button</button>
+        </section>
+
+        <section className='main_hometask'>Домашние задания</section>
+
+        <section className='main_teachers_report'>Отчеты от учителей</section>
+        
+        <section className='main_remaining_lessons'>
+          <h2 className='main_remaining_lessons_title'>Баланс занятий</h2>
+          <ul className='main_remaining_lessons_list'>
+            <li>
+              <p className='main_remaining_lessons_name'></p>
+              <div className='main_remaining_lessons_count'></div>
+            </li>
+            <li>
+              <p className='main_remaining_lessons_name'></p>
+              <div className='main_remaining_lessons_count'></div>
+            </li>
+            <li>
+              <p className='main_remaining_lessons_name'></p>
+              <div className='main_remaining_lessons_count'></div>
+            </li>
+          </ul>
+        </section>
+        <section></section>
       </main>
     </>
   );
