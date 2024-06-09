e7b1250b0b342fd3acbe3710d047179b9319da53
metastasio <myaldzina.m@gmail.com>
2024-06-09 14:52:02 +0300

    Applying styles to Main blocks

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -30,25 +30,90 @@ export const MainPage = () => {
         <section className='main_hometask'>Домашние задания</section>
 
         <section className='main_teachers_report'>Отчеты от учителей</section>
-        
+
         <section className='main_remaining_lessons'>
           <h2 className='main_remaining_lessons_title'>Баланс занятий</h2>
           <ul className='main_remaining_lessons_list'>
             <li>
-              <p className='main_remaining_lessons_name'></p>
-              <div className='main_remaining_lessons_count'></div>
+              <p className='main_remaining_lessons_name'>
+                Ментальная Арифметика
+              </p>
+              <div className='main_remaining_lessons_count'>32</div>
             </li>
             <li>
-              <p className='main_remaining_lessons_name'></p>
-              <div className='main_remaining_lessons_count'></div>
+              <p className='main_remaining_lessons_name'>Программирование</p>
+              <div className='main_remaining_lessons_count'>0</div>
             </li>
             <li>
-              <p className='main_remaining_lessons_name'></p>
-              <div className='main_remaining_lessons_count'></div>
+              <p className='main_remaining_lessons_name'>Скорочтение</p>
+              <div className='main_remaining_lessons_count'>4</div>
+            </li>
+          </ul>
+          <button className='main_remaining_lessons_button'>Button</button>
+        </section>
+
+        <section className='main_upcoming_lessons'>
+          <h2>Ближайшие уроки</h2>
+          <ul className='main_upcoming_lessons_list'>
+            <li>
+              <div className='main_upcoming_lessons_date'>
+                <p className='main_upcoming_lessons_day'>1</p>
+                <p className='main_upcoming_lessons_month'>мая</p>
+              </div>
+              <p className='main_upcoming_lessons_name'>
+                Ментальная Арифметика
+              </p>
+              <div className='main_upcoming_lessons_details'>
+                <p className='main_upcoming_lessons_details_time'>
+                  14:00-14:25
+                </p>
+                <p className='main_upcoming_lessons_details_name'>
+                  Белкина Инна
+                </p>
+              </div>
+              <div className='main_upcoming_lessons_buttons'>
+                <button>Button</button>
+                <button className='active'>Button</button>
+              </div>
+            </li>
+            <li>
+              <div className='main_upcoming_lessons_date'>
+                <p className='main_upcoming_lessons_day'>30</p>
+                <p className='main_upcoming_lessons_month'>октября</p>
+              </div>
+              <p className='main_upcoming_lessons_name'>Программирование</p>
+              <div className='main_upcoming_lessons_details'>
+                <p className='main_upcoming_lessons_details_time'>
+                  11:00-11:11
+                </p>
+                <p className='main_upcoming_lessons_details_name'>
+                  Животновская Оксана
+                </p>
+              </div>
+              <div className='main_upcoming_lessons_buttons'>
+                <button>Button</button>
+                <button className='active'>Button</button>
+              </div>
+            </li>
+            <li>
+              <div className='main_upcoming_lessons_date'>
+                <p className='main_upcoming_lessons_day'>16</p>
+                <p className='main_upcoming_lessons_month'>ноября</p>
+              </div>
+              <p className='main_upcoming_lessons_name'>Скорочтение</p>
+              <div className='main_upcoming_lessons_details'>
+                <p className='main_upcoming_lessons_details_time'>
+                  09:00-09:45
+                </p>
+                <p className='main_upcoming_lessons_details_name'>Мин Елена</p>
+              </div>
+              <div className='main_upcoming_lessons_buttons'>
+                <button>Button</button>
+                <button className='active'>Button</button>
+              </div>
             </li>
           </ul>
         </section>
-        <section></section>
       </main>
     </>
   );
